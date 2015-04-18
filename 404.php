eb1b32f6319e9f27f1b7db655c65750bb95fe540
Mc kenneth <mckenneth@me.com>
2015-04-18 12:27:19 -0700

    404
    added 404 error page

---
+++ 404.php
@@ -0,0 +1,12 @@
+<? include('header.php') ?>
+	<?php include('inc/side.php') ?>
+	<main role="main" class="main <?php echo $main_class ?>">
+	<div class="error404">
+	<h1>error: 404</h1>
+	<p>Sorry, the page you requested doesn't exist.</p>
+	<p class="small">Please head back to the <a href="/portfolio">homepage</a> or
+	kindly <a href="mailto:mckenneth@me.com">contact me</a> if this is a case of a broken link.</p>
+	</div>
+
+	</main>
+<? include('footer.php') ?>
